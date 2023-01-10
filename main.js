c436ac42bf001d327cd71b549f068ac077dbd1a5
Michał Gajda <misiek.krk@gmail.com>
2023-01-11 00:17:34 +0100

    prettier

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-import './sass/main.scss'
+import "./sass/main.scss";
 
 let options = {
-    strings: ['FRONTEND developer', 'BACKEND developer', 'BIKE rider', 'MTB rider'],
-    typeSpeed: 120,
-    startDelay: 500,
-    loop: true,
-    loopCount: Infinity,
+  strings: [
+    "FRONTEND developer",
+    "BACKEND developer",
+    "BIKE rider",
+    "MTB rider",
+  ],
+  typeSpeed: 120,
+  startDelay: 500,
+  loop: true,
+  loopCount: Infinity,
 };
 
-let typed = new Typed('.typed', options);
+let typed = new Typed(".typed", options);
